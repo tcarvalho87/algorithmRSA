ac371e045d300ff745f0f4c1a233fe031da8e101
Thiago Carvalho <tcarvalho87@gmail.com>
2019-11-19 14:51:30 -0400

    Gera p e q, calcula n

--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
 
 using namespace std;
 
+/*
+Gera um numero primo aleatório
+*/
+bool isPrime(int numEntrada){
+
+    if(numEntrada > 1 && !(numEntrada%2==0 || numEntrada%3==0 || numEntrada%5==0 || numEntrada%7==0)
+       || (numEntrada == 2 || numEntrada == 3 || numEntrada == 5 || numEntrada == 7)) {
+        return true;
+       }
+    else {
+        return false;
+    }
+}
+/*
+Gera um numero primo aleatório
+*/
+int generate_prime(){ //generate the prime number - p e q
+
+    int numeroPrimo = 0;
+    while(true) {
+        numeroPrimo = rand() % 1000 - 1; //define the range of the primes
+        if(isPrime(numeroPrimo))
+            return numeroPrimo;
+    }
+}
+
+/* Escolhe dois numeros primos grandes e calcula n*/
+long int calculaN(){
+    int p, q;
+
+    p = generate_prime();
+    q = generate_prime();
+    printf("\np: %d q:%d n:%ld",p,q,p*q);
+    return p*q;
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    long int n = calculaN();
+
     return 0;
 }
